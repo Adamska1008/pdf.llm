054e1ee3512fac8b528d2e478539e873eebf98bb
Zijian Zang <2639980868@qq.com>
2024-10-29 20:11:26 +0800

    feat: intercept default werkzeug logger

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+import sys
+import logging
+import re
+from flask import Flask, request
 from flask_cors import CORS
+from loguru import logger
 from .config import Config
 from .routes import api_blueprint
+
+
+logger.remove()
+logger.add(sys.stdout)
+
+
+class WerkzeugInterceptHandler(logging.Handler):
+    def emit(self, record):
+        message = record.getMessage()
+        # 正则提取请求方法和路径信息
+        match = re.search(
+            r'"(GET|POST|PUT|DELETE|PATCH|OPTIONS|HEAD) (.*?) (HTTP/\d.\d)" (\d{3})',
+            message,
+        )
+        if match:
+            method, path, http_version, status_code = match.groups()
+            logger.info(f"{method} {path} {http_version} {status_code}")
+        else:
+            logger.info(record.getMessage())
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app)
 app.register_blueprint(api_blueprint, url_prefix="/api")
+logging.getLogger("werkzeug").handlers = [WerkzeugInterceptHandler()]
