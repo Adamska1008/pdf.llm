bdcd6769e486036ceed48ffa019e5691265c5b39
Zijian Zang <2639980868@qq.com>
2024-10-30 22:37:36 +0800

    feat: optimize prompt build

--- backend/agent.py
+++ backend/agent.py
@@ -18,6 +18,8 @@ from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.callbacks import get_openai_callback
 from loguru import logger
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 class RagChatBot:
@@ -25,16 +27,46 @@ class RagChatBot:
     The AI model which make full use of rag
     """
 
+    class RagPromptBuilder:
+        def __init__(
+            self,
+            question: str,
+            page_number: str | None = None,
+            selected_text: list[str] | None = None,
+            context: str | None = None,
+            pages: list[Document] | None = None,
+        ):
+            if not question:
+                assert "prompt must require a question"
+            self._question = question
+            self._page_number = page_number
+            self._selected_text = selected_text
+            self._context = context
+            self._pages = pages
+
+        def build(self) -> str:
+            res = self._question
+            if self._page_number:
+                if not self._pages:
+                    assert "requires pages info, but not provided"
+                page_number = self._page_number
+                res += f"\n用户正在看{page_number}页，该页的内容为：\n{self._pages[page_number-1]}\n"
+            if self._selected_text:
+                res += f"用户引用了文本：{self._selected_text}\n"
+            if self._context:
+                res += f"相关上下文：{self._context}"
+            return res
+
     def __init__(self) -> None:
         self._augmented_with: str | None = None
         self._msgs: list[BaseMessage] = []
-        self._rag_usermsg_prompt = PromptTemplate.from_template(
-            "根据提供的问题，解决用户提出的问题。\n"
-            "问题: {question}"
-            "用户正在看第{page_number}页，请从第{page_number}页上寻找相关信息。"
-            "用户选中了文本，如下所示：\n{selected_text}\n请在解决问题时参考这一部分的内容。"
-            "上下文：{context}"
-        )
+        # self._rag_usermsg_prompt = PromptTemplate.from_template(
+        #     "根据提供的问题，解决用户提出的问题。\n"
+        #     "问题: {question}"
+        #     "用户正在看第{page_number}页，请从第{page_number}页上寻找相关信息。"
+        #     "用户选中了文本，如下所示：\n{selected_text}\n请在解决问题时参考这一部分的内容。"
+        #     "上下文：{context}"
+        # )
         self._prompt = ChatPromptTemplate.from_messages(
             [
                 SystemMessage(""),
@@ -47,6 +79,7 @@ class RagChatBot:
         self._vectorstore = None
         self._page_number = None
         self._selected_snippets = None
+        self._pages = None
 
     @property
     def _chain(self):
@@ -58,10 +91,12 @@ class RagChatBot:
             return
         self._augmented_with = file_path
         loader = PyPDFLoader(file_path)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
         pages = [page for page in loader.load()]
-
+        self._pages = pages
+        blocks = text_splitter.split_documents(pages)
         self._vectorstore = InMemoryVectorStore.from_documents(
-            pages, OpenAIEmbeddings()
+            blocks, OpenAIEmbeddings()
         )
 
     def focus_on_page(self, page_number: int):
@@ -90,17 +125,18 @@ class RagChatBot:
             if self.selected_text:
                 question_with_selected_text = question + self.selected_text
                 docs = self._vectorstore.similarity_search(
-                    question_with_selected_text, k=2
+                    question_with_selected_text, k=3
                 )
             else:
-                docs = self._vectorstore.similarity_search(question, k=2)
+                docs = self._vectorstore.similarity_search(question, k=3)
             # build `input`
-            return self._rag_usermsg_prompt.format(
+            return self.RagPromptBuilder(
                 question=question,
-                context=docs,
                 page_number=self._page_number,
                 selected_text=self.selected_text,
-            )
+                context=docs,
+                pages=self._pages,
+            ).build()
 
     def ask(self, question: str) -> str:
         inp = self._generate_input(question)
